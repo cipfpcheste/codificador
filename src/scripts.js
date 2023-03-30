eaba9293636420e7c30070f427f61708e78bcd3c
furriol <a.furriolguzman@edu.gva.es>
2023-03-30 09:27:15 +0200

    Separar Letras

--- src/scripts.js
+++ src/scripts.js
@@ -1,36 +1,64 @@
+
+
+const ArrayRegistro = [];
+const ArrayRegistro1 = [];
+
+
 function recogerDatos(){
 
-    let ArrayRegistro = [];
+    //while (ArrayRegistro.length > 0){
+    //    ArrayRegistro.pop();
+    //}
+    console.log(ArrayRegistro);   
     ArrayRegistro.push(document.getElementById('cursoFamilia').value);
-    console.log(ArrayRegistro);
-
+    
     ArrayRegistro.push(document.getElementById('familiaF').value);
-    console.log(ArrayRegistro);
+   
 
     ArrayRegistro.push(document.getElementById('familiaCF').value);
-    console.log(ArrayRegistro);
+    
 
     ArrayRegistro.push(document.getElementById('familiaMod').value);
-    console.log(ArrayRegistro);
+   
 
     ArrayRegistro.push(document.getElementById('profEv').value);
-    console.log(ArrayRegistro);
+    
 
     ArrayRegistro.push(document.getElementById('profRe').value);
-    console.log(ArrayRegistro);
+    console.log(ArrayRegistro);   
     
-    const PalArray = ArrayRegistro;
-    const SplitString = PalArray.split();
-
-    console.log(SplitString);
-    
-    //console.log(ArrayRegistro.toString())
-
-        
+   
 }
 
 function generarCodigo(){
 
+    const codigoGenerado = ArrayRegistro.join(",");
+    //codigoGenerado.split(" ");
+
+    document.getElementById('input1').value = codigoGenerado;
+    console.log(codigoGenerado);
+
+    for (let i=0; i<codigoGenerado.length;i++){
+        const element = codigoGenerado[i].toString().substring(0, 4);
+        ArrayRegistro1.push(element);   
+    }
+    console.log(ArrayRegistro1);
+    
+   
+const array = ["Juan,25,Madrid", "María,30,Barcelona", "Pedro,40,Sevilla"];
+const newArray = [];
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i].substring(0, 4); // Obtener los primeros cuatro caracteres de cada string
+  newArray.push(element);
+}
+
+console.log(newArray);
+// Output: ["Juan", "Marí", "Pedr"]
+
+
 
 }
+
+
 
